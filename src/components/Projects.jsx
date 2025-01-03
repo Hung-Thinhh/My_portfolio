8cccfc842fbf1471b0d80ab3bd87f506187695ac
Hung-Thinhh <hungthinhh2003@gmail.com>
2025-01-04 00:19:44 +0700

    ok img

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -35,14 +35,14 @@ const Projects = () => {
             data-aos="fade-left"
             spaceBetween={20}
             modules={[Pagination]}
-            className="rounded-3xl pb-16 max-w-4xl  drop-shadow-primary self-start"
+            className="rounded-3xl pb-16 max-w-2xl  drop-shadow-primary self-start"
           >
             {Projects.project_content.map((content, i) => (
               <SwiperSlide
                 key={i}
                 className="bg-white rounded-3xl p-5  border-b-8 border-[#FAF9FD] h-fit"
               >
-                <img src={content.image} alt="..." className="w-[100%]"/>
+                <img src={content.image} alt="..." className="w-[100%] rounded-md"/>
                 <div className="flex flex-col gap-1 mt-2">
                   <h5 className="font-bold font-Poppins">{content.title}</h5>
                   <a href={content.link} target="_blank" className="font-bold text-gray self-end">
